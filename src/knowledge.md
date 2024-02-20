73038fd128aa5308a1e1bc266adc02fedd1dc917
ErauQsPhan <mrsplol102@gmail.com>
2024-02-20 18:25:11 +0700

    next/image

--- src/knowledge.md
+++ src/knowledge.md
@@ -6,3 +6,15 @@
 - Link
 - Link URL Object
 - Route có đường dẫn tên cụ thể sẽ ưu tiên hơn slug
+
+# next/image
+
+- Tại sao cần sử dụng next/image?
+- Cách sử dụng local images
+- Tại sao next/image yêu cầu sử dụng width và height(CLS)
+- LQIP
+- placeholder image
+- Remote image
+- Nếu không biết width và height thì sử dụng ra sao?
+- Cách resposive hình ảnh với fill và lưu ý
+- Tìm hiểu srcset và sizes trong next/image
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
-import { LayoutMain } from "@/components/layouts";
+import Image from "next/image";
+import Butterfly from "@public/butterfly.jpg";
 
 export default function Home() {
-  return <LayoutMain>Homepage</LayoutMain>;
+  return (
+    <>
+      <img src="/butterfly.jpg" alt="butterfly" />
+      <div className="relative w-full h-full">
+        <Image src={Butterfly} alt="" fill></Image>
+      </div>
+    </>
+  );
 }
